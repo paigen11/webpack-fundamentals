4a8f686f64f592938507eaf49a9a3e5e820e3c3d
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-01-29 18:22:11 -0500

    loaders

---
+++ utils.js
@@ -0,0 +1,2 @@
+// this is a global js file provided to all apps
+console.log('logging from the utils.js file...')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-    entry: "./app.js", // the file entry point
+    entry: ["./app.js", "./utils.js"], // the file entry point
     output: {
         filename: "bundle.js" // name of bundled file
     },
-    watch: true // this has webpack watch the entry file for changes and auto rebuild when it sees them
+    watch: true ,// this has webpack watch the entry file for changes and auto rebuild when it sees them
+
+    module : {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "jshint-loader"
+            }
+
+        ],
+        loaders: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [' ', '.js', '.es6'] // the extensions means babel will look for either a .js file or a .es6 file and not freak out
+
+    }
 };
